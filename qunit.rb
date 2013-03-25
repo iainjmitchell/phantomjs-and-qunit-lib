55f4ed7d729f0003dff09716bf3f8d52b1002642
Iain Mitchell <iainjmitchell@gmail.com>
2013-03-25 13:11:15 +0000

    adding windows exe for phantom js and making rb task cross platform compatible

--- qunit.rb
+++ qunit.rb
@@ -2,13 +2,23 @@ class QunitConfiguration
 	attr_accessor :phantom_exe, :qunit_runner, :test_directory
 end
 
-def qunit(*args, &block)
-	configuration = QunitConfiguration.new;
-	task = Proc.new{ 
-		p "Qunit starting..."
-		block.call(configuration)
-		sh "#{configuration.phantom_exe} #{configuration.qunit_runner} #{configuration.test_directory}/*.html"
-		p "Qunit run completed"
-	};
+def qunit(*args, &block)	
+	qunit_wrapper = Qunit_Wrapper.new(&block)
+	task = Proc.new { qunit_wrapper.run }
 	Rake::Task.define_task(*args, &task)
+end
+
+class Qunit_Wrapper
+	def initialize(&block)
+		@block = block;
+	end
+	
+	def run()			
+		config = QunitConfiguration.new		
+		@block.call(config)		
+		Dir.glob("#{config.test_directory}/*.html") do |test_file|			
+			puts "testing  #{test_file}"	
+			system "\"#{config.phantom_exe}\" \"#{config.qunit_runner}\" \"#{test_file}\""  		
+		end						
+	end
 end
